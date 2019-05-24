fb8fd8e1e8906673270694c1ded0d0ee60983fa0
roohtajsingh <roohtaj.singh@gmail.com>
2019-05-24 14:15:12 -0400

    second comit

---
+++ src/parttime/java.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parttime;
+
+/**
+ *
+ * @author Gulneer Sidhu
+ */
+public class java {
+
+    private int numberofhrs;
+
+    /**
+     * Get the value of numberofhrs
+     *
+     * @return the value of numberofhrs
+     */
+    public int getNumberofhrs() {
+        return numberofhrs;
+    }
+
+    /**
+     * Set the value of numberofhrs
+     *
+     * @param numberofhrs new value of numberofhrs
+     */
+    public void setNumberofhrs(int numberofhrs) {
+        this.numberofhrs = numberofhrs;
+    }
+
+    
+}
